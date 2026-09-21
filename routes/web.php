1d30e4f9c6add3f991cc20f18b24dfaa01122331
John Kenly Pamor <johnkenlypamor13@gmail.com>
2026-09-21 17:36:13 +0800

    fixed case sensitive naming convention

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ require __DIR__.'/auth.php';
 require __DIR__.'/adminAuth.php';
 require __DIR__.'/destinationRoute.php';
 require __DIR__.'/hotelRoute.php';
-require __DIR__.'/RoomRoute.php';
+require __DIR__.'/roomRoute.php';
 require __DIR__.'/activityRoute.php';
 require __DIR__.'/userManagementRoute.php';
 require __DIR__.'/inventoryRoute.php';
